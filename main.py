53c5785f8de316f09d5c7685833d4a37a629b609
Tricked111 <Shift3114@gmail.com>
2022-07-03 11:04:35 +0200

    create start command

---
+++ main.py
@@ -0,0 +1,25 @@
+""" todo telgram bot 
+autor: Tricked111 """
+
+from telebot import types,TeleBot
+from datetime import date
+
+API_TOKEN = ""
+bot = TeleBot(API_TOKEN)
+
+@bot.message_handler(commands = ['start'])
+def start_command_handler(message: types.Message):
+    bot.send_message(message.chat.id,
+                     f"Hello {message.from_user.first_name}" 
+                     "\nThis is TODO bot for you."
+                     "\nPress /todo to start.")
+
+@bot.message_handler(commands = ['todo'])
+def todo_command_handler(message: types.Message):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
